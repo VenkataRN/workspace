019424f8450ac3034550d2324b3b0bd53b403b1d
VenkataRN <Venkat.RN@gmail.com>
2022-12-20 20:41:34 +0000

    Base Clone
    Base clone

---
+++ onference/src/main/java/Application.java
@@ -0,0 +1,9 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+
+public class Application {
+    public static void main (String[] args) {
+        SpeakerService speakerService = new SpeakerServiceImpl();
+        System.out.println(speakerService.findAll().get(0).getFirstName());
+    }
+}
