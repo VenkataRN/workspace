5741e8dddb106511e2ada34ffa9944ef404c2945
VenkataRN <Venkat.RN@gmail.com>
2023-01-13 22:31:26 +0000

    XML Configuration setter Injection byName

--- onference-xml/src/main/java/Application.java
+++ onference-xml/src/main/java/Application.java
@@ -14,7 +14,7 @@ public class Application {
         System.out.println("XML Based Spring Application");
         System.out.println("Now integrated with applicationContext.xml");
         System.out.println("Used Setter Injection (Constructor is not called)  here but XML is autowired configuration");
-        System.out.println("Used autowire=byType  in XML configuration for setter injection");
+        System.out.println("Used autowire=byName  in XML configuration for setter injection");
         System.out.println(speakerService.findAll().get(0).getFirstName());
     }
 }
@@ -24,6 +24,6 @@ Setter Injection
 XML Based Spring Application
 Now integrated with applicationContext.xml
 Used Setter Injection (Constructor is not called)  here but XML is autowired configuration
-Used autowire=byType  in XML configuration for setter injection
+Used autowire=byName  in XML configuration for setter injection
 Venkat
  */
